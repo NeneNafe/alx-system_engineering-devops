84d79ff12a6ca1d473422fac1a25056170ef1bff
NeneNafe <nenechiwaya@gmail.com>
2023-10-31 22:13:52 +0200

    Revised

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,4 @@
 #!/usr/bin/env ruby
 # A ruby script that accepts one argument and pass
 # to a regular expression matching method
-
-if ARGV.length != 1
-  puts "Usage: #{$0} <string>"
-  exit(1)
-end
-
-input_str = ARGV[0]
-
-regex = /School/
-
-matches = input_str.scan(regex)
-
-puts matches.join
+puts ARGV[0].scan(/School/).join
