4577cedad951668546b2c9a38bda2019bb7f57d2
NeneNafe <nenechiwaya@gmail.com>
2023-10-31 21:15:44 +0200

    Corrections

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,15 +2,4 @@
 # a Ruby script that accepts one argument and pass
 #+ it to a regular expression matching method
 
-if ARGV.length != 1
-  puts "Usage: #{$0} <string>"
-  exit(1)
-end
-
-input_str = ARGV[0]
-
-regex = /h+b+t/
-
-matches = input_str.scan(regex)
-
-puts matches.join
+puts ARGV[0].scan(/hbt{2, 5}n/).join
