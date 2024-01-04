c5b3e9ff71f0cd263c6fd35e84aabed425848cd8
NeneNafe <nenechiwaya@gmail.com>
2024-01-04 22:07:32 +0200

    Puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# puppet manifest to make changes to config file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+content =>"
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+}
