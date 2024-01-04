956173f3003787ac9bc1bb922c3478bacbf3eb56
NeneNafe <nenechiwaya@gmail.com>
2024-01-04 22:10:26 +0200

    Puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # puppet manifest to make changes to config file
 
-file { 'etc/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
 	ensure => present,
 content =>"
 	#SSH client configuration
