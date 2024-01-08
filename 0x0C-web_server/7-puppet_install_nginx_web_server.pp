e934a69ca2e9950874acd715dc4f40df9897e6ed
NeneNafe <nenechiwaya@gmail.com>
2024-01-08 19:16:31 +0200

    Pupper nginx install

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Uses Puppet to install nginx
+
+package { 'nginx':
+  ensure => present,
+}
+
+file_line {'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'redirect_me https:\/\/github.com\/NeneNafe permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'].
+}
