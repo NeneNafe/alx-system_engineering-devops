67383d79d829f76c671c1dca905f42f414481e90
NeneNafe <nenechiwaya@gmail.com>
2024-01-08 19:19:54 +0200

    Corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,7 +8,7 @@ file_line {'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'redirect_me https:\/\/github.com\/NeneNafe permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/NeneNafe permanent;',
 }
 
 file { '/var/www/html/index.html':
