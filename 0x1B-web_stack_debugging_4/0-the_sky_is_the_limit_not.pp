f0bbaaaea89b0ad832174ce7c31ca1f17f7ee72b
NeneNafe <nenechiwaya@gmail.com>
2024-02-22 10:40:10 +0200

    Corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # This script fixes the server error using nginx
 
 file { 'fix-for-nginx':
-	ensure  => 'file',
-	path    => '/etc/default/nginx',
-	content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
+ensure  => 'file',
+path    => '/etc/default/nginx',
+content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
 }
 
 -> exec { 'nginx-restart':
-	command => 'service nginx restart',
-	path    => '/etc/init.d/',
+command => 'service nginx restart',
+path    => '/etc/init.d/',
 }
